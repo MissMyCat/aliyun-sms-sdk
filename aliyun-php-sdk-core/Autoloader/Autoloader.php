51b8a26fc31f645bf398003e1b7d8b08aca8eb3b
jonny77 <943626451@qq.com>
2020-11-24 10:31:01 +0800

    Update Autoloader.php
    composer2.0不兼容了, 微调了一下

--- aliyun-php-sdk-core/Autoloader/Autoloader.php
+++ aliyun-php-sdk-core/Autoloader/Autoloader.php
@@ -18,11 +18,8 @@
  * under the License.
  */
 namespace Mrgoon\AliyunSmsSdk\AutoLoader;
-
-spl_autoload_register("Autoloader::autoload");
-class Autoloader
-{
-	private static $autoloadPathArray = array(
+spl_autoload_register(function (){
+	    $autoloadPathArray = array(
 		"aliyun-php-sdk-core",
 		"aliyun-php-sdk-core/Auth",
 		"aliyun-php-sdk-core/Http",
@@ -30,23 +27,12 @@ class Autoloader
 		"aliyun-php-sdk-core/Regions",
 		"aliyun-php-sdk-core/Exception"
 	);
-	
-	public static function autoload($className)
-	{
-		foreach (self::$autoloadPathArray as $path) {
-			$file = dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR.$path.DIRECTORY_SEPARATOR.$className.".php";
-			$file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
-			if(is_file($file)){
-				include_once $file;
-				break;
-			}
+	foreach ($autoloadPathArray as $path) {
+		$file = dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR.$path.DIRECTORY_SEPARATOR.$className.".php";
+		$file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
+		if(is_file($file)){
+			include_once  $file;
+			break;
 		}
 	}
-	
-	public static function addAutoloadPath($path)
-	{
-		array_push(self::$autoloadPathArray, $path);
-	}
-}
-
-?>
+});
